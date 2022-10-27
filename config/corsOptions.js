be646afb3cbc69957b3d70e785eb811cfbaa2d07
Or mizrahi <108184274+OrMizrahi12@users.noreply.github.com>
2022-10-27 09:43:03 +0300

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,10 +1,6 @@
 const allowedOrigins = require('./allowedOrigins');   
-//#19 
-// we import the list, and chaek if the domain is allowed
-
 const corsOptions = {
     origin: (origin, callback) => {
-        // if the domain exsist in the list, we will send true!
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
@@ -15,5 +11,3 @@ const corsOptions = {
 }
 
 module.exports = corsOptions;
-
-// for #20 go to credentials ---> 
