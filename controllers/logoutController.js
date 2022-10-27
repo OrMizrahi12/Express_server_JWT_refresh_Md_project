2ff799cc61312dc978d05db8b1352add8cd84517
Or mizrahi <108184274+OrMizrahi12@users.noreply.github.com>
2022-10-27 09:45:27 +0300

    Update logoutController.js

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,30 +1,22 @@
-//#15
-// this controller check if the user and token exsist.
-// if yes - the token will remove! 
+
 const User = require('../model/User'); 
 
 const handleLogout = async (req, res) => {
-    // On client, also delete the accessToken
-
+    
     const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(204); //No content
+    if (!cookies?.jwt) 
+        return res.sendStatus(204); //No content
     const refreshToken = cookies.jwt;
-
-    // Is refreshToken in db?
     const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
         return res.sendStatus(204);
     }
-
-    // Delete refreshToken in db
     foundUser.refreshToken = '';
     const result = await foundUser.save();
     console.log(result);
-
     res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     res.sendStatus(204);
 }
-// for //#16 go to logout.js
 
 module.exports = { handleLogout }
