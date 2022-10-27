2ee3cae68f403205c5c1a46fdb51897b3eecd54a
Or mizrahi <108184274+OrMizrahi12@users.noreply.github.com>
2022-10-27 09:47:02 +0300

    Update registerController.js

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,17 +3,14 @@ const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
-    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
-
-    // check for duplicate usernames in the db
+    if (!user || !pwd)
+        return res.status(400).json({ 'message': 'Username and password are required.' });
     const duplicate = await User.findOne({ username: user }).exec();
-    if (duplicate) return res.sendStatus(409); //Conflict 
-
+    if (duplicate) 
+        return res.sendStatus(409); //Conflict 
     try {
-        //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
 
-        //create and store the new user
         const result = await User.create({
             "username": user,
             "password": hashedPwd
